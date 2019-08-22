e43c82f56628e91087104776d9aee5be2efb2564
Minseok <song.minseok@gmail.com>
2019-08-22 09:37:50 +0900

    Material Theme 적용

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+import Box from '@material-ui/core/Box'
+
+const useStyles = makeStyles((theme) => ({
+  root: { flexGrow: 1, padding: theme.spacing(1) },
+  copyright: {
+    flexGrow: 1,
+    margin: theme.spacing(1),
+    textAlign: 'left',
+    marginRight: theme.spacing(10),
+  },
+  link: {
+    margin: theme.spacing(1),
+  },
+}))
 
 export default function Footer() {
+  const classes = useStyles()
   return (
-    <div>
-      <hr />
-      <p>먹는 약을 등록하고 관리할 수 있습니다. Copyright by Minseok </p>
-    </div>
+    <Typography className={classes.root}>
+      <Box className={classes.copyright} component='span'>
+        {' '}
+        Copyright by Minseok{' '}
+      </Box>
+      <Link className={classes.link} href='https://www.google.com'>
+        Google
+      </Link>
+      <Link className={classes.link} href='https://www.facebook.com'>
+        Facebook
+      </Link>
+      <Link className={classes.link} href='https://www.naver.com'>
+        Naver
+      </Link>
+    </Typography>
+    // <div>
+    //   <hr />
+    //   <p>먹는 약을 등록하고 관리할 수 있습니다. Copyright by Minseok </p>
+    // </div>
   )
 }
--- src/App/Topbar.js
+++ src/App/Topbar.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import LocalPharmacyIcon from '@material-ui/icons/LocalPharmacy'
+import PersonIcon from '@material-ui/icons/Person'
+
+const useStyles = makeStyles((theme) => ({
+  root: { flexGrow: 1 },
+  homeButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: { flexGrow: 1 },
+}))
 
 export default function Topbar() {
+  const classes = useStyles()
+
   return (
-    <div>
-      <h1>YakMoa</h1>
-      <hr />
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.homeButton}
+            color='inherit'
+            aria-label='home'
+          >
+            <LocalPharmacyIcon />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            약모아
+          </Typography>
+          <Button color='inherit'>Login</Button>
+          <PersonIcon />
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
