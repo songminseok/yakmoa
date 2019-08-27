9eb38ccd9750e67ddab163642008d7d352ebfba0
Minseok <song.minseok@gmail.com>
2019-08-27 09:14:47 +0900

    layout initial work

--- src/App/App.js
+++ src/App/App.js
@@ -3,10 +3,12 @@ import './App.css'
 import Topbar from './Topbar'
 import Main from './Main'
 import Footer from './Footer'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 function App() {
   return (
     <div className='App'>
+      <CssBaseline />
       <Topbar />
       <Main />
       <Footer />
--- src/App/Topbar.js
+++ src/App/Topbar.js
@@ -3,15 +3,43 @@ import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
 import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import LocalPharmacyIcon from '@material-ui/icons/LocalPharmacy'
-import PersonIcon from '@material-ui/icons/Person'
+import Box from '@material-ui/core/Box'
+import logo from '../assets/topbar_logo.png'
+import appDownIco from '../assets/ico_app_down.svg'
 
 const useStyles = makeStyles((theme) => ({
   root: { flexGrow: 1 },
+  appBar: {
+    height: '96px',
+    color: 'black',
+    background: 'transparent',
+    boxShadow: 'none',
+    position: 'absolute',
+  },
+  logo: {
+    width: '254px',
+    height: '33px',
+    objectFit: 'contain',
+  },
   homeButton: {
     marginRight: theme.spacing(2),
+  },
+  link: {
+    margin: theme.spacing(3),
+    color: '#2c2f31',
+    fontSize: 18,
+    fontWeight: 'bold',
+    '&:focus, &:hover, &:active': {
+      color: '#17d3d6',
+      textDecoration: 'none',
+    },
+  },
+  buttonAppDown: {
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
   },
   title: { flexGrow: 1 },
 }))
@@ -21,22 +49,34 @@ export default function Topbar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.homeButton}
-            color='inherit'
-            aria-label='home'
-          >
-            <LocalPharmacyIcon />
-          </IconButton>
-          <Typography variant='h6' className={classes.title}>
-            약모아
-          </Typography>
-          <Button color='inherit'>Login</Button>
-          <PersonIcon />
-        </Toolbar>
+      <AppBar className={classes.appBar}>
+        <Box my='auto' pl={16} pr={16} clone>
+          <Toolbar>
+            {/* <CardMedia className={classes.logo} image={logo} /> */}
+            <Link className={classes.logo} href='/'>
+              <img className={classes.logo} src={logo} alt='Refone X CU X Kt' />
+            </Link>
+            <Box className={classes.title} component='span'>
+              {' '}
+            </Box>
+            <Typography component='div'>
+              <Link className={classes.link} href='https://refone.co.kr'>
+                리폰소개
+              </Link>
+              <Link
+                className={classes.link}
+                href='https://refone.co.kr/device/list'
+              >
+                시세조회
+              </Link>
+              <Link className={classes.link}>매장안내</Link>
+              <Link className={classes.link}>이벤트</Link>
+            </Typography>
+            <Button className={classes.buttonAppDown} disableRipple>
+              <img src={appDownIco} alt='App Download' />
+            </Button>
+          </Toolbar>
+        </Box>
       </AppBar>
     </div>
   )
