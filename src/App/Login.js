d11df7f14857636fcb85bd30e31cc38dc80b9de6
Minseok Song <song.minseok@gmail.com>
2019-09-02 16:48:43 +0900

    로그인, 회원가입 화면

--- src/App/Login.js
+++ src/App/Login.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+    flexGrow: '1',
+  },
+}));
 
 export default function Login() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h3>로그인하기</h3>
-    </div>
-  )
+    <Grid container spacing={2} direction='column' alignContent='center'>
+      <Grid item xs={4}>
+        <Typography variant='h3'>Log in to YakMoa</Typography>
+      </Grid>
+      <Grid item xs={4}>
+        <FormControl className={classes.margin} fullWidth>
+          <InputLabel htmlFor='input-username'>Email</InputLabel>
+          <Input id='input-username' type='email' />
+          <FormHelperText id='input-username-helper-text'>
+            이메일
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={4}>
+        <FormControl className={classes.margin} fullWidth>
+          <InputLabel htmlFor='input-password'>Password</InputLabel>
+          <Input id='input-password' />
+          <FormHelperText id='input-assword-helper-text'>
+            비밀번호
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={4}>
+        <FormControl className={classes.margin} fullWidth>
+          <Button variant='contained' color='primary'>
+            Log In
+          </Button>
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
 }
--- src/App/Signup.js
+++ src/App/Signup.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+    flexGrow: '1',
+  },
+}));
 
 export default function Signup() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h3>회원가입하기</h3>
-    </div>
-  )
+    <Grid container spacing={2} direction='column' alignContent='center'>
+      <Grid item xs={4}>
+        <Typography variant='h3'>Sign up to YakMoa</Typography>
+      </Grid>
+      <Grid item xs={4}>
+        <FormControl className={classes.margin} fullWidth>
+          <InputLabel htmlFor='input-username'>Email</InputLabel>
+          <Input id='input-username' />
+          <FormHelperText id='input-username-helper-text'>
+            이메일
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={4}>
+        <FormControl className={classes.margin} fullWidth>
+          <InputLabel htmlFor='input-password'>Password</InputLabel>
+          <Input id='input-password' />
+          <FormHelperText id='input-password-helper-text'>
+            비밀번호
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={4}>
+        <FormControl className={classes.margin} fullWidth>
+          <InputLabel htmlFor='input-password-confirm'>
+            Confirm Password
+          </InputLabel>
+          <Input id='input-password-confirm' />
+          <FormHelperText id='input-password-confirm-helper-text'>
+            비밀번호 확인
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={4}>
+        <FormControl className={classes.margin} fullWidth>
+          <Button variant='contained' color='primary'>
+            Sign Up
+          </Button>
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
 }
