bb24ff921ad5ead4be9e141bcadf97bf498b47d4
Minseok Song <song.minseok@gmail.com>
2019-09-05 19:58:22 +0900

    firebase setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,48 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import './App.css'
-import Topbar from './Topbar'
-import Intro from './Intro'
-import Login from './Login'
-import Signup from './Signup'
-import Footer from './Footer'
-import CssBaseline from '@material-ui/core/CssBaseline'
+import './App.css';
+import Topbar from './Topbar';
+import Intro from './Intro';
+import Login from './Login';
+import Signup from './Signup';
+import Footer from './Footer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from 'firebase/app';
+
+// Add the Firebase products that you want to use
+import 'firebase/auth';
+import 'firebase/database';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDE9-USX4OY8Afh5dGeDhX8N1Lt-28p6Ds',
+  authDomain: 'yakmoa-80569.firebaseapp.com',
+  databaseURL: 'https://yakmoa-80569.firebaseio.com',
+  projectId: 'yakmoa-80569',
+  storageBucket: 'yakmoa-80569.appspot.com',
+  messagingSenderId: '692085812753',
+  appId: '1:692085812753:web:65aae7b8840d046e',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <CssBaseline />
       <Router>
         <Topbar />
-        <Route path='/' exact component={Intro} />
-        <Route path='/login' component={Login} />
-        <Route path='/signup' component={Signup} />
+        <Route path="/" exact component={Intro} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
       </Router>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
