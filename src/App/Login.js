7e49bea9f33ccdb79266d856a22c73dabbc1ae4b
Minseok <song.minseok@gmail.com>
2019-09-06 20:08:33 +0900

    login validation 코드 정리

--- src/App/Login.js
+++ src/App/Login.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from 'react'
 /** firebase */
-import * as firebase from 'firebase';
+import * as firebase from 'firebase'
 
-import { makeStyles } from '@material-ui/core/styles';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles'
+import FormControl from '@material-ui/core/FormControl'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
 
 /** yup - for validation */
-import * as yup from 'yup';
+import * as yup from 'yup'
 
+/** 로그인 스키마 */
 let schema = yup.object().shape({
   password: yup
     .string()
@@ -26,43 +27,36 @@ let schema = yup.object().shape({
   // createdOn: yup.date().default(function() {
   //   return new Date();
   // }),
-});
-
-const yupOptions = {
-  abortEarly: true,
-};
+})
 
 const useStyles = makeStyles((theme) => ({
   margin: {
     margin: theme.spacing(1),
     flexGrow: '1',
   },
-}));
+}))
 
 export default function Login() {
-  const classes = useStyles();
+  const classes = useStyles()
   const [loginInfo, setLoginInfo] = React.useState({
     email: '',
     password: '',
-  });
+  })
   const [inputError, setInputError] = React.useState({
     email: '',
     password: '',
-  });
-  const loginButton = React.useRef(null);
+  })
+  const [currentUser, setCurrentUser] = React.useState(null)
 
-  function handleChange(e) {
-    const newLoginInfo = {
-      ...loginInfo,
-      [e.target.name]: e.target.value,
-    };
-    setLoginInfo(newLoginInfo);
-    validateInput(e.target, newLoginInfo);
-  }
+  React.useEffect(() => {
+    return firebase.auth().onAuthStateChanged(function(user) {
+      setCurrentUser(user)
+    })
+  }, [])
 
   function updateInputError(error) {
-    const newInputError = { ...inputError, ...error };
-    setInputError(newInputError);
+    const newInputError = { ...inputError, ...error }
+    setInputError(newInputError)
   }
 
   function validateInput(input, login) {
@@ -70,71 +64,63 @@ export default function Login() {
       .validateAt(input.name, login)
       .then(function(valid) {
         if (valid) {
-          updateInputError({ [input.name]: '' });
+          updateInputError({ [input.name]: '' })
         }
       })
       .catch(function(error) {
-        updateInputError({ [input.name]: error.message });
-      });
+        updateInputError({ [input.name]: error.message })
+      })
   }
 
-  function handleBlur(e) {
-    validateInput(e.target, loginInfo);
+  function signOut(e) {
+    firebase.auth().signOut()
   }
   /**
    * Handles the sign in button press.
    */
-  function toggleSignIn(e) {
-    e.preventDefault();
+  function signIn(e) {
+    const { email, password } = loginInfo
 
-    const { email, password } = loginInfo;
-
-    if (firebase.auth().currentUser) {
-      // [START signout]
-      console.log('signOut if a user is still logged in');
-      firebase.auth().signOut();
-      // [END signout]
-    } else {
-      // validation with yup
-      schema
-        .validate(loginInfo, yupOptions)
-        .then(function(valid) {
-          console.log('loginInfo is ', valid ? 'valid' : 'invalid');
-          firebase
-            .auth()
-            .signInWithEmailAndPassword(email, password)
-            .then(function(user) {
-              const userlog = JSON.stringify(user, 2);
-              alert(`Successfully logged in ${userlog}`);
-              console.log('logged in ', user);
-            })
-            .catch(function(error) {
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              // [START_EXCLUDE]
-              if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password.');
-              } else {
-                alert(errorMessage);
-              }
-              console.log(error);
-              loginButton.current.disabled = false;
-              // [END_EXCLUDE]
-            });
-        })
-        .catch(function(error) {
-          console.log(error);
-          setInputError({ [error.path]: error.message });
-        });
-
-      // [END authwithemail]
-    }
-    loginButton.current.disabled = true;
+    // validation with yup
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(function(user) {
+        const userlog = JSON.stringify(user, 2)
+        // alert(`Successfully logged in ${userlog}`)
+        console.log('logged in ', userlog)
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code
+        var errorMessage = error.message
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/wrong-password') {
+          alert('Wrong password.')
+        } else {
+          alert(errorMessage)
+        }
+        console.log(error)
+        // [END_EXCLUDE]
+      })
+    // [END authwithemail]
   }
 
-  return (
-    <Grid container spacing={2} direction='column' alignContent='center'>
+  function handleChange(e) {
+    const newLoginInfo = {
+      ...loginInfo,
+      [e.target.name]: e.target.value,
+    }
+    setLoginInfo(newLoginInfo)
+    validateInput(e.target, newLoginInfo)
+  }
+
+  function handleBlur(e) {
+    validateInput(e.target, loginInfo)
+  }
+
+  const loginInForm = (
+    <>
       <Grid item xs={4}>
         <Typography variant='h3'>Log in to YakMoa</Typography>
       </Grid>
@@ -176,10 +162,9 @@ export default function Login() {
       <Grid item xs={4}>
         <FormControl className={classes.margin} fullWidth>
           <Button
-            ref={loginButton}
             variant='contained'
             color='primary'
-            onClick={toggleSignIn}
+            onClick={signIn}
             disabled={
               !(
                 loginInfo.email &&
@@ -193,6 +178,28 @@ export default function Login() {
           </Button>
         </FormControl>
       </Grid>
+    </>
+  )
+
+  return (
+    <Grid container spacing={2} direction='column' alignContent='center'>
+      {currentUser != null ? (
+        <>
+          <Grid item xs={4}>
+            <Typography variant='h5'>{currentUser.email}</Typography>
+            <br />
+            <br />
+            <Typography style={{ height: '300px' }}>
+              Successfully logged In
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Button onClick={signOut}>Log Out</Button>
+          </Grid>
+        </>
+      ) : (
+        loginInForm
+      )}
     </Grid>
-  );
+  )
 }
