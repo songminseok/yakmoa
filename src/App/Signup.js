a60f06f85db55949b93eae0b0bcee62e2adcdd49
Minseok <song.minseok@gmail.com>
2019-09-08 12:29:50 +0900

    signup dialog

--- src/App/Signup.js
+++ src/App/Signup.js
@@ -7,6 +7,11 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
 
 /** firebase */
 import * as firebase from 'firebase';
@@ -39,7 +44,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Signup() {
+export default function Signup(props) {
   const classes = useStyles();
   const [signupInfo, setSignUpInfo] = React.useState({
     email: '',
@@ -59,7 +64,7 @@ export default function Signup() {
         .createUserWithEmailAndPassword(email, password)
         .then(function(user) {
           console.log('Successfully signed up');
-          alert('Successfully signed up');
+          setUser(user);
         })
         .catch(function(error) {
           // Handle Errors here.
@@ -97,91 +102,126 @@ export default function Signup() {
     validateInput(name, value);
   }
 
+  function handleClose(e) {
+    setUser(null);
+  }
+
+  function handleCloseAndLogin() {
+    handleClose();
+    props.history.push('/');
+  }
+
   return (
-    <Grid container spacing={2} direction='column' alignContent='center'>
-      <Grid item xs={4}>
-        <Typography variant='h3'>Sign up to YakMoa</Typography>
+    <>
+      <Grid container spacing={2} direction='column' alignContent='center'>
+        <Grid item xs={4}>
+          <Typography variant='h3'>Sign up to YakMoa</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <FormControl className={classes.margin} fullWidth>
+            <InputLabel htmlFor='email'>Email</InputLabel>
+            <Input
+              id='email'
+              name='email'
+              type='email'
+              value={signupInfo.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <FormHelperText
+              id='email-helper-text'
+              error={!!signupInfo.errors.email}
+            >
+              {!signupInfo.errors.email ? '이메일' : signupInfo.errors.email}
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={4}>
+          <FormControl className={classes.margin} fullWidth>
+            <InputLabel htmlFor='password'>Password</InputLabel>
+            <Input
+              id='password'
+              name='password'
+              type='password'
+              value={signupInfo.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <FormHelperText
+              id='password-helper-text'
+              error={!!signupInfo.errors.password}
+            >
+              {!signupInfo.errors.password
+                ? '비밀번호'
+                : signupInfo.errors.password}
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={4}>
+          <FormControl className={classes.margin} fullWidth>
+            <InputLabel htmlFor='passwordConfirm'>Confirm Password</InputLabel>
+            <Input
+              id='passwordConfirm'
+              name='passwordConfirm'
+              type='password'
+              value={signupInfo.passwordConfirm}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <FormHelperText
+              id='passwordConfirm-helper-text'
+              error={!!signupInfo.errors.passwordConfirm}
+            >
+              {!signupInfo.errors.passwordConfirm
+                ? '비밀번호 확인'
+                : signupInfo.errors.passwordConfirm}
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={4}>
+          <FormControl className={classes.margin} fullWidth>
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={signUp}
+              disabled={
+                !signupInfo.email ||
+                !signupInfo.password ||
+                !signupInfo.passwordConfirm ||
+                !!signupInfo.errors.email ||
+                !!signupInfo.errors.password ||
+                !!signupInfo.errors.passwordConfirm
+              }
+            >
+              Sign Up
+            </Button>
+          </FormControl>
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <FormControl className={classes.margin} fullWidth>
-          <InputLabel htmlFor='email'>Email</InputLabel>
-          <Input
-            id='email'
-            name='email'
-            type='email'
-            value={signupInfo.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          <FormHelperText
-            id='email-helper-text'
-            error={!!signupInfo.errors.email}
-          >
-            {!signupInfo.errors.email ? '이메일' : signupInfo.errors.email}
-          </FormHelperText>
-        </FormControl>
-      </Grid>
-      <Grid item xs={4}>
-        <FormControl className={classes.margin} fullWidth>
-          <InputLabel htmlFor='password'>Password</InputLabel>
-          <Input
-            id='password'
-            name='password'
-            type='password'
-            value={signupInfo.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          <FormHelperText
-            id='password-helper-text'
-            error={!!signupInfo.errors.password}
-          >
-            {!signupInfo.errors.password
-              ? '비밀번호'
-              : signupInfo.errors.password}
-          </FormHelperText>
-        </FormControl>
-      </Grid>
-      <Grid item xs={4}>
-        <FormControl className={classes.margin} fullWidth>
-          <InputLabel htmlFor='passwordConfirm'>Confirm Password</InputLabel>
-          <Input
-            id='passwordConfirm'
-            name='passwordConfirm'
-            type='password'
-            value={signupInfo.passwordConfirm}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          <FormHelperText
-            id='passwordConfirm-helper-text'
-            error={!!signupInfo.errors.passwordConfirm}
-          >
-            {!signupInfo.errors.passwordConfirm
-              ? '비밀번호 확인'
-              : signupInfo.errors.passwordConfirm}
-          </FormHelperText>
-        </FormControl>
-      </Grid>
-      <Grid item xs={4}>
-        <FormControl className={classes.margin} fullWidth>
-          <Button
-            variant='contained'
-            color='primary'
-            onClick={signUp}
-            disabled={
-              !signupInfo.email ||
-              !signupInfo.password ||
-              !signupInfo.passwordConfirm ||
-              !!signupInfo.errors.email ||
-              !!signupInfo.errors.password ||
-              !!signupInfo.errors.passwordConfirm
-            }
-          >
-            Sign Up
+      {/** Dialog */}
+      <Dialog
+        open={!!user}
+        onClose={handleClose}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <DialogTitle id='alert-dialog-title'>
+          {'회원 가입에 성공했어요.😀'}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id='alert-dialog-description'>
+            축하합니다. 약모아 회원 가입에 성공했습니다. 홈으로 이동해 주세요.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color='primary'>
+            닫기
           </Button>
-        </FormControl>
-      </Grid>
-    </Grid>
+          <Button onClick={handleCloseAndLogin} color='primary' autoFocus>
+            홈으로 가기
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
